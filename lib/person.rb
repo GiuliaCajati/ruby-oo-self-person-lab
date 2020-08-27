61c2f5e114cb6d1f4f4f2d710d8c9c70499b0390
Melindadiaz07 <melindadiaz@me.com>
2020-08-27 15:05:50 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,48 +1,28 @@
 class Person 
     
-    
-    
     def initialize(name)
         @name = name
         @bank_account = 25
         @happiness = 8
         @hygiene = 8
-
     end
 
-   
 
-
-    attr_accessor :hygiene, :happiness, :bank_account 
+    attr_accessor :hygiene, :happiness, :bank_account, :take_bath
     attr_reader :name
     
 
-    def happiness=(happiness)
-        if happiness < 0 
-            @happiness = 0
-        elsif happiness > 10
-            @happiness = 10
-        else 
-            @happiness = happiness
-        end
+    def happiness=(new_happiness)
+        @happiness = new_happiness.clamp(0, 10)
     end
 
-    def hygiene=(hygiene)
-        if hygiene < 0 
-            @hygiene = 0
-        elsif hygiene > 10
-            @hygiene = 10
-        else 
-            @hygiene = hygiene
-        end
+    def hygiene=(new_hygiene)
+        @hygiene=new_hygiene.clamp(0, 10)
     end
-
-
 
     def clean?
         @hygiene > 7
     end
-
 
     def happy?
          @happiness > 7
@@ -51,17 +31,39 @@ class Person
 
     def get_paid(salary)
         @bank_account = @bank_account + salary
-
         return "all about the benjamins"
     end
 
-    def take_bath
-        @hygiene = @hygiene + 4
-        hygiene=(@hygiene)
-
-        returns '♪ Rub-a-dub just relaxing in the tub ♫'
+    def take_bath #new_person.take_bath
+        self.hygiene = self.hygiene + 4
+         '♪ Rub-a-dub just relaxing in the tub ♫'
     end
     
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(callee)
+        self.happiness += 3
+        callee.happiness += 3
+        "Hi #{callee.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(other_person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            other_person.happiness -= 2
+           'blah blah partisan blah lobbyist'
+        elsif topic == "weather"
+            self.happiness += 1
+            other_person.happiness += 1
+            'blah blah sun blah rain'
+        else
+            'blah blah blah blah blah'
+        end
+    end
 
 end
 
